52ee4c1bfe5b7254d11044fbc2e4b90a3f31834b
ruslan <sonikx94@yandex.ru>
2023-05-23 23:22:00 +0300

    Задание 3

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -1,3 +1,6 @@
+from fractions import Fraction
+
+
 # 2. Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
 # Функцию hex используйте для проверки своего результата.
 def task2():
@@ -40,7 +43,43 @@ def task2():
 # 3. Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
 # Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
 def task3():
-    pass
+    first_frac = input("Введите первую дробь:\n").split(sep='/')
+    second_frac = input("Введите вторую дробь:\n").split(sep='/')
+
+    first_numerator = int(first_frac[0])
+    first_denominator = int(first_frac[1])
+    second_numerator = int(second_frac[0])
+    second_denominator = int(second_frac[1])
+
+    check_first_frac = Fraction(first_numerator, first_denominator)
+    check_second_frac = Fraction(second_numerator, second_denominator)
+
+    print(f'Произведение дробей: {simplify_fraction(first_numerator * second_numerator, first_denominator * second_denominator)}')
+    print(f'Произведение дробей (Fraction): {check_first_frac * check_second_frac}')
+
+    if first_denominator != second_denominator:
+        tmp = first_denominator
+        first_numerator *= second_denominator
+        first_denominator *= second_denominator
+        second_numerator *= tmp
+        second_denominator *= tmp
+
+    print(f'Сумма дробей: {simplify_fraction(first_numerator + second_numerator, first_denominator)}')
+    print(f'Сумма дробей (Fraction): {check_first_frac + check_second_frac}')
+
+
+def simplify_fraction(numerator: int, denominator: int) -> str:
+    val = denominator
+    while val > 1:
+        if numerator % val == 0 and denominator % val == 0:
+            numerator //= val
+            denominator //= val
+            val = denominator
+            continue
+
+        val -= 1
+
+    return f'{numerator}/{denominator}'
 
 
 task2()
